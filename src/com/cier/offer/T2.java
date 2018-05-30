5fe517f4168a61a47133edfcc42c56dec9d0b1e7
liuenci <liuencier@gmail.com>
2018-05-30 21:26:27 +0800

    新增两个第二题的解法暴力简单

--- src/com/cier/offer/T2.java
+++ src/com/cier/offer/T2.java
@@ -5,7 +5,7 @@ package com.cier.offer;
  * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
  */
 public class T2 {
-    public static String replaceSpace(StringBuffer str) {
+    public static String replaceSpace1(StringBuffer str) {
         StringBuffer stringBuffer = new StringBuffer();
         char[] chars = str.toString().toCharArray();
         for (int i = 0; i < chars.length; i++) {
@@ -19,6 +19,23 @@ public class T2 {
     }
 
     public static void main(String[] args) {
-        System.out.println(replaceSpace(new StringBuffer("We Are Happy")));
+        System.out.println(replaceSpace3(new StringBuffer("We Are Happy")));
     }
+
+    public static String replaceSpace2(StringBuffer str) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' '){
+                stringBuffer.append("%20");
+            }else{
+                stringBuffer.append(str.charAt(i));
+            }
+        }
+        return stringBuffer.toString();
+    }
+
+    public static String replaceSpace3(StringBuffer str) {
+        return str.toString().replaceAll("\\s","%20");
+    }
+
 }
