ecdd5ae07e6b6e2000fe7348b6851180aadf3f3a
liuenci <liuencier@gmail.com>
2018-06-04 21:19:32 +0800

    提交第五题

---
+++ src/com/cier/offer/T5.java
@@ -0,0 +1,23 @@
+package com.cier.offer;
+
+import java.util.Stack;
+
+public class T5 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        int first = stack2.pop();
+        while(!stack2.isEmpty()){
+            stack1.push(stack2.pop());
+        }
+        return first;
+    }
+}
