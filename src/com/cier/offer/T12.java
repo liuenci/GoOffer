50b30ff1385d9ac92b8c315a125e5d5534826d59
liuenci <liuencier@gmail.com>
2018-06-19 17:05:50 +0800

    传统计算

--- src/com/cier/offer/T12.java
+++ src/com/cier/offer/T12.java
@@ -1,7 +1,21 @@
 package com.cier.offer;
 
 public class T12 {
+    /**
+     * 最菜的菜鸡会用这种方法1
+     * @param base
+     * @param exponent
+     * @return
+     */
     public double Power(double base, int exponent) {
         return Math.pow(base,exponent);
+    }
+
+    public double Power1(double base, int exponent) {
+        double temp = 1;
+        for (int i = 1;i <= Math.abs(exponent);i++){
+            temp *= base;
+        }
+        return exponent < 0 ? (1/temp) :temp;
     }
 }
