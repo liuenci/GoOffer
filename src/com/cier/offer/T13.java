f0d2cd8c7a3866c337bd12653b816f3182b2e315
liuenci <liuencier@gmail.com>
2018-06-23 11:03:30 +0800

    提交第 13 题

---
+++ src/com/cier/offer/T13.java
@@ -0,0 +1,22 @@
+package com.cier.offer;
+
+/**
+ * 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，
+ * 使得所有的奇数位于数组的前半部分，所有的偶数位于位于数组的后半部分，
+ * 并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+ */
+public class T13 {
+
+    public void reOrderArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] % 2 == 0 && array[j + 1] % 2 == 1) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+}
