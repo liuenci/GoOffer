b4e858c51714dac5375bc565fb2812158e372cc0
liuenci <liuencier@gmail.com>
2018-07-03 11:04:53 +0800

    从上往下打印二叉树

---
+++ src/com/cier/offer/T22.java
@@ -0,0 +1,31 @@
+package com.cier.offer;
+
+import java.util.ArrayList;
+
+/**
+ * 从上往下打印出二叉树的每个节点，同层节点从左至右打印。
+ */
+public class T22 {
+
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<TreeNode> queue = new ArrayList<>();
+        if (root == null) {
+            return arrayList;
+        }
+        queue.add(root);
+        while (queue.size() != 0){
+            TreeNode temp = queue.remove(0);
+            if (temp.left != null) {
+                queue.add(temp.left);
+            }
+            if (temp.right != null) {
+                queue.add(temp.right);
+            }
+            arrayList.add(temp.val);
+        }
+        return arrayList;
+    }
+
+
+}
