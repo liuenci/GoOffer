f33e06f1faf77a671b2753e48e36fe290ed0a2f3
liuenci <liuencier@gmail.com>
2018-07-05 10:05:32 +0800

    二叉树中和为某一值的路径

---
+++ src/com/cier/offer/T24.java
@@ -0,0 +1,26 @@
+package com.cier.offer;
+
+import java.util.ArrayList;
+
+/**
+ * 输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
+ */
+public class T24 {
+    private ArrayList<ArrayList<Integer>> listAll = new ArrayList<ArrayList<Integer>>();
+    private ArrayList<Integer> lists = new ArrayList<>();
+
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+        if (root == null) {
+            return listAll;
+        }
+        lists.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null) {
+            listAll.add(new ArrayList<>(lists));
+        }
+        FindPath(root.left, target);
+        FindPath(root.right, target);
+        lists.remove(lists.size() - 1);
+        return listAll;
+    }
+}
