95b85e8104e14c2930947bbf1fe511969fe8a833
liuenci <liuencier@gmail.com>
2018-10-11 08:50:38 +0800

    复杂链表的复制

---
+++ src/com/cier/offer/RandomListNode.java
@@ -0,0 +1,11 @@
+package com.cier.offer;
+
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
