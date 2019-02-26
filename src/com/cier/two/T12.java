3aace417e581b3312fc0ea117bff18fe3d1095d3
liuenci <liuencier@gmail.com>
2019-02-26 19:44:13 +0800

    T12

---
+++ src/com/cier/two/T12.java
@@ -0,0 +1,10 @@
+package com.cier.two;
+
+/**
+ * https://www.nowcoder.com/practice/1a834e5e3e1a4b7ba251417554e07c00?tpId=13&tqId=11165&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class T12 {
+    public double Power(double base, int exponent) {
+        return Math.pow(base,exponent);
+    }
+}
