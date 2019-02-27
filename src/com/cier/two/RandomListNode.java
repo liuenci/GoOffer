b62bcbb7b4b04242413930706d345db50837ca5a
liuenci <liuencier@gmail.com>
2019-02-27 20:48:38 +0800

    todolist

---
+++ src/com/cier/two/RandomListNode.java
@@ -0,0 +1,11 @@
+package com.cier.two;
+
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
---
+++ src/com/cier/two/T25.java
@@ -0,0 +1,10 @@
+package com.cier.two;
+
+/**
+ * https://www.nowcoder.com/practice/f836b2c43afc4b35ad6adc41ec941dba?tpId=13&tqId=11178&rp=2&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class T25 {
+    public RandomListNode Clone(RandomListNode pHead) {
+        return null;
+    }
+}
