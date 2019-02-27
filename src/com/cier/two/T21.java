152e50caf8b30e43118d91d52882927573447454
liuenci <liuencier@gmail.com>
2019-02-27 17:57:47 +0800

    todolist

---
+++ src/com/cier/two/T21.java
@@ -0,0 +1,12 @@
+package com.cier.two;
+
+/**
+ * https://www.nowcoder.com/practice/d77d11405cc7470d82554cb392585106?tpId=13&tqId=11174&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ * 1 2 3 4 5
+ * 2 1 5 4 3
+ */
+public class T21 {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        return false;
+    }
+}
