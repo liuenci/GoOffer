18fa2b43b2ab834ac90c46e8ef67bfaa777cdafe
liuenci <liuencier@gmail.com>
2019-02-27 11:49:54 +0800

    T18

---
+++ src/com/cier/two/T17.java
@@ -0,0 +1,10 @@
+package com.cier.two;
+
+/**
+ * https://www.nowcoder.com/practice/6e196c44c7004d15b1610b9afca8bd88?tpId=13&tqId=11170&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class T17 {
+    public boolean HasSubtree(TreeNode root1,TreeNode root2) {
+        return false;
+    }
+}
