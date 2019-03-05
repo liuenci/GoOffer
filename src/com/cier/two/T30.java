e29e37cb9b14bc5dff976878a322a728339378d9
liuenci <liuencier@gmail.com>
2019-03-05 17:33:39 +0800

    todolist

---
+++ src/com/cier/two/T30.java
@@ -0,0 +1,14 @@
+package com.cier.two;
+
+/**
+ * @ClassName T30
+ * @Author liuenci
+ * @Date 2019/3/5 下午5:30
+ * @Version 1.0
+ * https://www.nowcoder.com/practice/459bd355da1549fa8a49e350bf3df484?tpId=13&tqId=11183&rp=2&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ */
+public class T30 {
+    public int FindGreatestSumOfSubArray(int[] array) {
+        return 0;
+    }
+}
