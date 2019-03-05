b580fa65cab4cdc2474935599e6271c203934a23
liuenci <liuencier@gmail.com>
2019-03-05 18:42:14 +0800

    T4

--- src/com/cier/two/T4.java
+++ src/com/cier/two/T4.java
@@ -11,6 +11,22 @@ package com.cier.two;
  */
 public class T4 {
     public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
-        return null;
+        TreeNode root = reConstructBinaryTree(pre,0,pre.length - 1,in,0,in.length - 1);
+        return root;
+    }
+
+    private TreeNode reConstructBinaryTree(int[] pre,int startPre, int endPre, int[] in, int startIn, int endIn){
+        if (startPre > endPre || startIn > endIn) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[startPre]);
+        for (int i = startIn; i <= endIn; i++) {
+            if (in[i] == pre[startPre]) {
+                root.left = reConstructBinaryTree(pre,startPre + 1,startPre + i - startIn, in, startIn, i - 1);
+                root.right = reConstructBinaryTree(pre, startPre + 1 + i - startIn, endPre, in, i + 1, endIn);
+                break;
+            }
+        }
+        return root;
     }
 }
