54f02ff91055b4ea28df0aa4e89837c70df20142
liuenci <liuencier@gmail.com>
2019-03-14 15:03:15 +0800

    update solution

---
+++ autocommit.sh
@@ -0,0 +1,4 @@
+#### 自动提交脚本
+git add README.md
+git commit -m "update readme.md"
+git push -u origin master
--- src/com/cier/two/T17.java
+++ src/com/cier/two/T17.java
@@ -5,6 +5,22 @@ package com.cier.two;
  */
 public class T17 {
     public boolean HasSubtree(TreeNode root1,TreeNode root2) {
-        return false;
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        return isSubtreeWithRoot(root1,root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+    }
+
+    private boolean isSubtreeWithRoot(TreeNode root1, TreeNode root2) {
+        if (root2 == null) {
+            return true;
+        }
+        if (root1 == null) {
+            return false;
+        }
+        if (root1.val != root2.val) {
+            return false;
+        }
+        return isSubtreeWithRoot(root1.left, root2.left) && isSubtreeWithRoot(root1.right, root2.right);
     }
 }
