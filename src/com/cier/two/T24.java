cd5b8dd9afaa3fdfd37ecc0c5cdb323867769cbb
liuenci <liuencier@gmail.com>
2019-03-14 17:50:37 +0800

    update solution

--- src/com/cier/two/T24.java
+++ src/com/cier/two/T24.java
@@ -6,8 +6,24 @@ import java.util.ArrayList;
  * https://www.nowcoder.com/practice/b736e784e3e34731af99065031301bca?tpId=13&tqId=11177&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
  */
 public class T24 {
-
+    ArrayList<ArrayList<Integer>> list = new ArrayList<>();
     public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
-        return null;
+        backtracking(root,target, new ArrayList<>());
+        return list;
+    }
+
+    private void backtracking(TreeNode node, int target, ArrayList<Integer> path) {
+        if (node == null) {
+            return;
+        }
+        path.add(node.val);
+        target -= node.val;
+        if (target == 0 && node.left == null && node.right == null) {
+            list.add(new ArrayList<>(path));
+        } else {
+            backtracking(node.left, target, path);
+            backtracking(node.right, target, path);
+        }
+        path.remove(path.size() - 1);
     }
 }
