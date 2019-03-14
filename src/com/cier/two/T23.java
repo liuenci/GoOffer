b78a5c7bc03b8b8056f9a6356d279ecc058756c2
liuenci <liuencier@gmail.com>
2019-03-14 17:15:42 +0800

    update solution

--- src/com/cier/two/T23.java
+++ src/com/cier/two/T23.java
@@ -1,26 +1,35 @@
 package com.cier.two;
 
-import java.util.Stack;
-
 /**
  * https://www.nowcoder.com/practice/a861533d45854474ac791d90e447bafd?tpId=13&tqId=11176&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
  *   2
  *  1 3
  *
+ * 1 3 2
  * @author liuenci
  */
 public class T23 {
     public boolean VerifySquenceOfBST(int [] sequence) {
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < sequence.length; i++) {
-            stack.push(sequence[i]);
+        if (sequence == null || sequence.length == 0) {
+            return false;
         }
-
-        return false;
+        return verify(sequence,0,sequence.length - 1);
     }
 
-    public static void main(String[] args) {
-
-
+    private boolean verify(int[] sequence,int first,int last){
+        if (last - first <= 1) {
+            return true;
+        }
+        int rootVal = sequence[last];
+        int curIndex = first;
+        while (curIndex < last && sequence[curIndex] <= rootVal) {
+            curIndex++;
+        }
+        for (int i = curIndex; i < last; i++) {
+            if (sequence[i] < rootVal) {
+                return false;
+            }
+        }
+        return verify(sequence,first, curIndex - 1) && verify(sequence, curIndex, last - 1);
     }
 }
