8ee5cd06eb8630308c9c6b11676897e6f2f1e159
liuenci <liuencier@gmail.com>
2019-03-14 16:14:42 +0800

    update solution

--- src/com/cier/two/T21.java
+++ src/com/cier/two/T21.java
@@ -1,12 +1,29 @@
 package com.cier.two;
 
+import java.util.Stack;
+
 /**
  * https://www.nowcoder.com/practice/d77d11405cc7470d82554cb392585106?tpId=13&tqId=11174&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
  * 1 2 3 4 5
  * 2 1 5 4 3
  */
 public class T21 {
-    public boolean IsPopOrder(int [] pushA,int [] popA) {
-        return false;
+    public boolean IsPopOrder(int[] pushA, int[] popA) {
+        int n = pushA.length;
+        Stack<Integer> stack = new Stack<>();
+        for (int pushIndex = 0, popIndex = 0; pushIndex < n; pushIndex++) {
+            stack.push(pushA[pushIndex]);
+            while (popIndex < n && !stack.isEmpty() && stack.peek() == popA[popIndex]) {
+                stack.pop();
+                popIndex++;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        T21 t21 = new T21();
+
+        t21.IsPopOrder(new int[]{1,2,3,4,5},new int[]{2,1,5,4,3});
     }
 }
