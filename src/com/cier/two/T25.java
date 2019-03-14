3eb50559de557a5fa147c143fc2707974a3a758a
liuenci <liuencier@gmail.com>
2019-03-14 18:29:22 +0800

    update solution

--- src/com/cier/two/T25.java
+++ src/com/cier/two/T25.java
@@ -5,6 +5,34 @@ package com.cier.two;
  */
 public class T25 {
     public RandomListNode Clone(RandomListNode pHead) {
-        return null;
+        if (pHead == null) {
+            return null;
+        }
+        // 插入新节点
+        RandomListNode cur = pHead;
+        while (cur != null) {
+            RandomListNode clone = new RandomListNode(cur.label);
+            clone.next = cur.next;
+            cur.next = clone;
+            cur = clone.next;
+        }
+        // 建立 Random 连接
+        cur = pHead;
+        while (cur != null) {
+            RandomListNode clone = cur.next;
+            if (cur.random != null) {
+                clone.random = cur.random.next;
+            }
+            cur = clone.next;
+        }
+        // 拆分
+        cur = pHead;
+        RandomListNode pHeadClone = pHead.next;
+        while (cur.next != null) {
+            RandomListNode next = cur.next;
+            cur.next = next.next;
+            cur = next;
+        }
+        return pHeadClone;
     }
 }
